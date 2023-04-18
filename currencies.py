cfcd3b6a1d08dfdf8e932b8d6fec5a0d6da8df2c
Ariele Amorim <arieleamorim@Arieles-MacBook-Pro.local>
2023-04-18 11:42:42 +0200

    Change 2

--- currencies.py
+++ currencies.py
@@ -1,10 +1,7 @@
 # pylint: disable=missing-docstring
 
 # TODO: add some currency rates
-RATES = {'USDEUR': 0.85,
-         'GBPEUR': 1.13,
-         'CHFEUR': 0.86,
-         'EURGBP': 0.885}
+RATES = {'USDEUR': 0.85,'GBPEUR': 1.13, 'CHFEUR': 0.86,'EURGBP': 0.885}
 
 
 def convert(amount, currency):
